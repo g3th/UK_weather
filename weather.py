b6a4322d14a663f65ecaef6d47b7e0e6764a0286
g3th <theinkandpen@yahoo.co.uk>
2022-08-27 20:47:22 +0100

    modified:   weather.py

--- weather.py
+++ weather.py
@@ -5,24 +5,19 @@ from header import title
 from bs4 import BeautifulSoup as soup
 title()
 
-while True:
-	try:
-		postcode = input('\nPlease Enter a Valid UK Postcode: ')
-		location = 'https://www.doogal.co.uk/UKPostcodes.php?Search='
-		location_page=location+postcode[:3]
-		fetch_location = requests.get(location_page)
-		parse_location = soup(fetch_location.content, 'html.parser')
-		location= parse_location.find('p').text.split('areas of')[1].split('.')[0].replace(' ','').replace(',',', ')
-		page = 'https://weather-broker-cdn.api.bbci.co.uk/en/forecast/aggregated/{}'.format(postcode)
-		request = requests.get(page).json()
-		date = str(request['forecasts'][0]['detailed']['lastUpdated']).split('T')[0]
-		time = str(request['forecasts'][0]['detailed']['lastUpdated']).split('T')[1].split('.')[0]
-		break
-	except KeyError:
-		print ('Invalid Postcode.'); time.sleep(0.8)
-		print('\x1bc')
-		continue
+postcode = input('\nPlease Enter a Valid UK Postcode: ')
+location = 'https://www.doogal.co.uk/UKPostcodes.php?Search='
+location_page=location+postcode[:3]
+fetch_location = requests.get(location_page)
+parse_location = soup(fetch_location.content, 'html.parser')
+location= parse_location.find('p').text.split('areas of')[1].split('.')[0]
 
-print('Current weather for {}\n'.format(location))
+title()
+print('\nCurrent weather for{}\n'.format(location))
+page = 'https://weather-broker-cdn.api.bbci.co.uk/en/forecast/aggregated/{}'.format(postcode)
+request = requests.get(page).json()
+break
+
+print(request)
 
 
