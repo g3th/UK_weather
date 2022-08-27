b08bde9c88934e3be23fc2457fc856a8343cd24b
Roberto <mrrobertrausch@gmail.com>
2022-08-27 04:03:20 +0100

    new file:   header.py 	modified:   weather.py

--- weather.py
+++ weather.py
@@ -1,11 +1,18 @@
 import requests
 import json
 import time
+from header import title
 from bs4 import BeautifulSoup as soup
+title()
 
 while True:
 	try:
-		postcode = input('Please Enter a Valid UK Postcode: ')
+		postcode = input('\nPlease Enter a Valid UK Postcode: ')
+		location = 'https://www.doogal.co.uk/UKPostcodes.php?Search='
+		location_page=location+postcode[:3]
+		fetch_location = requests.get(location_page)
+		parse_location = soup(fetch_location.content, 'html.parser')
+		location= parse_location.find('p').text.split('areas of')[1].split('.')[0].replace(' ','').replace(',',', ')
 		page = 'https://weather-broker-cdn.api.bbci.co.uk/en/forecast/aggregated/{}'.format(postcode)
 		request = requests.get(page).json()
 		date = str(request['forecasts'][0]['detailed']['lastUpdated']).split('T')[0]
@@ -16,6 +23,6 @@ while True:
 		print('\x1bc')
 		continue
 
-print(request)
+print('Current weather for {}\n'.format(location))
 
 
