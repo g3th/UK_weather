696bb6db1d77417efa8316c89eeb186a7c6a0d32
g3th <theinkandpen@yahoo.co.uk>
2022-08-28 03:08:24 +0100

    modified:   weather.py

--- weather.py
+++ weather.py
@@ -3,11 +3,11 @@ import json
 import time
 from header import title
 from bs4 import BeautifulSoup as soup
-title()
-
-postcode = input('\nPlease Enter a Valid UK Postcode: ')
+#title()
+error_flag =1
+postcode = 'UB6 8JF' #input('\nPlease Enter a Valid UK Postcode: ')
 location = 'https://www.doogal.co.uk/UKPostcodes.php?Search='
-location_page=location+postcode[:3]
+location_page=location+postcode
 fetch_location = requests.get(location_page)
 parse_location = soup(fetch_location.content, 'html.parser')
 location= parse_location.find('p').text.split('areas of')[1].split('.')[0]
@@ -15,9 +15,18 @@ location= parse_location.find('p').text.split('areas of')[1].split('.')[0]
 title()
 print('\nCurrent weather for{}\n'.format(location))
 page = 'https://weather-broker-cdn.api.bbci.co.uk/en/forecast/aggregated/{}'.format(postcode)
-request = requests.get(page).json()
-break
+current_weather = requests.get(page).json()
 
-print(request)
+
+forecast_counter = 0
+
+
+print('Forecast: {} | Time Slot {}'.format(current_weather['forecasts'][0]['detailed']['reports'][0]['enhancedWeatherDescription'], current_weather['forecasts'][0]['detailed']['reports'][0]['timeslot']))
+print('Forecast: {} | Time Slot {}'.format(current_weather['forecasts'][0]['detailed']['reports'][0]['enhancedWeatherDescription'], current_weather['forecasts'][0]['detailed']['reports'][1]['timeslot']))
+print('Forecast: {} | Time Slot {}'.format(current_weather['forecasts'][0]['detailed']['reports'][0]['enhancedWeatherDescription'], current_weather['forecasts'][0]['detailed']['reports'][2]['timeslot']))
+print('Forecast: {} | Time Slot {}'.format(current_weather['forecasts'][1]['detailed']['reports'][0]['enhancedWeatherDescription'], current_weather['forecasts'][1]['detailed']['reports'][0]['timeslot']))
+print('Forecast: {} | Time Slot {}'.format(current_weather['forecasts'][1]['detailed']['reports'][1]['enhancedWeatherDescription'], current_weather['forecasts'][1]['detailed']['reports'][1]['timeslot']))
+print('Forecast: {} | Time Slot {}'.format(current_weather['forecasts'][1]['detailed']['reports'][2]['enhancedWeatherDescription'], current_weather['forecasts'][1]['detailed']['reports'][2]['timeslot']))
+
 
 
