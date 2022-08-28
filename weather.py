91c3aa46778ec18cda6ca8cb4543da2da59d547e
Roberto <mrrobertrausch@gmail.com>
2022-08-28 05:37:35 +0100

    modified:   weather.py

--- weather.py
+++ weather.py
@@ -1,32 +1,55 @@
 import requests
 import json
 import time
+from random import randint
 from header import title
 from bs4 import BeautifulSoup as soup
-#title()
-error_flag =1
-postcode = 'UB6 8JF' #input('\nPlease Enter a Valid UK Postcode: ')
-location = 'https://www.doogal.co.uk/UKPostcodes.php?Search='
-location_page=location+postcode
-fetch_location = requests.get(location_page)
-parse_location = soup(fetch_location.content, 'html.parser')
-location= parse_location.find('p').text.split('areas of')[1].split('.')[0]
+
+while True:
+	title()
+	try:
+		postcode = input('\nPlease Enter a Valid UK Postcode: ')
+		location = 'https://www.doogal.co.uk/UKPostcodes.php?Search='
+		location_page=location+postcode
+		fetch_location = requests.get(location_page)
+		parse_location = soup(fetch_location.content, 'html.parser')
+		location= parse_location.find('p').text.split('areas of')[1].split('.')[0]
+		break
+	except IndexError:
+		print('Invalid Postcode, Please Try Again.\n')
+		input('(Hit Enter)')
 
 title()
-print('\nCurrent weather for{}\n'.format(location))
+print('\n\33[38;5;226mCurrent weather for{}'.format(location))
+print('---------------------------------------------------')
 page = 'https://weather-broker-cdn.api.bbci.co.uk/en/forecast/aggregated/{}'.format(postcode)
 current_weather = requests.get(page).json()
 
+day = 0
+forecast_and_reports_counter = 0
+descriptions_counter = 0
 
-forecast_counter = 0
-
-
-print('Forecast: {} | Time Slot {}'.format(current_weather['forecasts'][0]['detailed']['reports'][0]['enhancedWeatherDescription'], current_weather['forecasts'][0]['detailed']['reports'][0]['timeslot']))
-print('Forecast: {} | Time Slot {}'.format(current_weather['forecasts'][0]['detailed']['reports'][0]['enhancedWeatherDescription'], current_weather['forecasts'][0]['detailed']['reports'][1]['timeslot']))
-print('Forecast: {} | Time Slot {}'.format(current_weather['forecasts'][0]['detailed']['reports'][0]['enhancedWeatherDescription'], current_weather['forecasts'][0]['detailed']['reports'][2]['timeslot']))
-print('Forecast: {} | Time Slot {}'.format(current_weather['forecasts'][1]['detailed']['reports'][0]['enhancedWeatherDescription'], current_weather['forecasts'][1]['detailed']['reports'][0]['timeslot']))
-print('Forecast: {} | Time Slot {}'.format(current_weather['forecasts'][1]['detailed']['reports'][1]['enhancedWeatherDescription'], current_weather['forecasts'][1]['detailed']['reports'][1]['timeslot']))
-print('Forecast: {} | Time Slot {}'.format(current_weather['forecasts'][1]['detailed']['reports'][2]['enhancedWeatherDescription'], current_weather['forecasts'][1]['detailed']['reports'][2]['timeslot']))
-
-
-
+while day < 7:
+	try:
+		if descriptions_counter < 1:
+			print('\r\33[38;5;202mThis is the forecast for {}'.format(current_weather['forecasts'][forecast_and_reports_counter]['detailed']['reports'][descriptions_counter]['localDate']),end='')
+			print('\n---------------------------------------------------')
+			error_flag = 0
+	except KeyError:
+		print('\r\33[38;5;{}mFetching Please Wait'.format(str(randint(0,255))), end='')
+		current_weather = requests.get(page).json()
+		error_flag = 1
+	if error_flag == 0:
+		try:		
+			print('\33[38;5;154mForecast: {} 		| Time Slot: {}	| Temp: {}'.format(current_weather['forecasts'][forecast_and_reports_counter]['detailed']['reports'][descriptions_counter]['enhancedWeatherDescription'], current_weather['forecasts'][forecast_and_reports_counter]['detailed']['reports'][descriptions_counter]['timeslot'], current_weather['forecasts'][forecast_and_reports_counter]['detailed']['reports'][descriptions_counter]['temperatureC']))
+			descriptions_counter +=1
+		except IndexError:
+			forecast_and_reports_counter += 1
+			descriptions_counter = 0
+			day += 1
+			input('\n\33[38;5;81mHit Enter for {}'.format(current_weather['forecasts'][forecast_and_reports_counter]['detailed']['reports'][descriptions_counter]['localDate']))
+			title()
+			print('\n\33[38;5;226mCurrent weather for{}'.format(location))
+			print('---------------------------------------------------')
+title()
+print ('\nThat was your 7 day forecast')
